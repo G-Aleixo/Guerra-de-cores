7177323c31ff0cf3d22cc41281a97ba375c86b88
G-Aleixo <163425479+G-Aleixo@users.noreply.github.com>
2024-10-25 00:00:00 +0000

    Make game actually playable

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import game
+import os
 
-board = game.Game(3)
+cls = lambda: os.system("cls") if os.name == "nt" else os.system("clear")
 
-board.setup_game([1, 2], [1, 1])
+board: game.Game = game.Game(6)
 
+pos1 = [int(indice) for indice in input("Onde o jogador 1 começara?\n").split()]
+pos2 = [int(indice) for indice in input("Onde o jogador 2 começara?\n").split()]
+
+board.setup_game(pos1, pos2)
+
+current_player: int = 1
+win: bool = False
+cls()
+while not win:
+    cls()
+    board.display_board()
+    
+    pos = [int(indice) for indice in input(f"Where will player {int(-current_player / 2 + 1.5)} play?\n").split()]
+    
+    board.add_point_to(current_player, pos)
+    current_player *= -1
+    
+    win, winning_player = board.has_won()
+cls()
 board.display_board()
-
-board.add_point_to(-1, [1, 1])
-
-board.display_board()
-
-print(board.has_won())
+print(f"Player {int(-current_player / 2 + 1.5)} has won!")
